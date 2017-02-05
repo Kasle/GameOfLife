7cdd41fabc58e66751770f0556f4793120f6b9b5
Aleks Mercik <aleks.mercik@outlook.com>
2017-02-05 11:06:48 -0500

    some stuff

--- life.py
+++ life.py
@@ -5,11 +5,11 @@ import pygame
 
 pygame.init()
 
-fps = 10
+fps = 20
 
 scale = 5
 
-n = 150
+n = 100
 
 clock = pygame.time.Clock()
 gameDisplay = pygame.display.set_mode([n*scale, n*scale])
@@ -17,7 +17,7 @@ gameDisplay = pygame.display.set_mode([n*scale, n*scale])
 cell = chr(219)
 
 f = [[" " for i in range(n)] for j in range(n)]
-for i in range(int((n/2)**2)):
+for i in range(int(n/2)**2):
     f[random.randint(0, n-1)][random.randint(0, n-1)] = cell
 
 def step(board):
@@ -26,40 +26,49 @@ def step(board):
     for x in range(n):
         for y in range(n):
             count = 0
-            for dx in [-1, 0, 1]:
-                for dy in [-1, 0, 1]:
+            shouldKill = False
+            rng = [-1, 0, 1]
+            for dx in rng:
+                for dy in rng:
                     a = dx
                     b = dy
+                    
                     if a == 0 == b:
                         continue
+
                     if x+a < 0:
                         a+=n
+                        shouldKill = True
                     elif x+a >= n:
                         a-=n
+                        shouldKill = True
                     if y+b < 0:
                         b+=n
+                        shouldKill = True
                     elif y+b >= n:
                         b-=n
+                        shouldKill = True
 
                     #print("At", x, y, ": '",board[x+a][y+b],"' at", x+a, y+b)
                     
                     if board[x+a][y+b] != " ":
                         count+=1
+
+            #if shouldKill:
+            #    board[x][y] = " "
             
             #print(x, y, count)
             if board[x][y] == cell:
-                if count in [0, 1]:
+                if count < 2:
                     nBoard[x][y] = " "
                     #print("removing at", x, y)
-                elif count in [4, 5, 6, 7, 8]:
-                    nBoard[x][y] = " "
-                    #print("removing at", x, y)
-                elif count in [2, 3]:
+                elif count < 4:
                     nBoard[x][y] = cell
+                    #print("removing at", x, y)
                 else:
-                    nBoard[x][y] = " " 
+                    nBoard[x][y] = " "
             else:
-                if count in [3]:
+                if count == 3:
                     nBoard[x][y] = cell
                     #print ("Adding at", x, y)
                 else:
@@ -71,12 +80,19 @@ gameExit = False
 
 ts = time.time()
 
+shouldTick = False
+
 while not gameExit:
     for event in pygame.event.get(): ##Event Handler
         if event.type == pygame.QUIT:
             gameExit = True
         elif event.type == 5:
             print("Mouse!")
+            p = event.pos
+            f[int(p[0]/scale)][int(p[1]/scale)] = cell
+        elif event.type == 2:
+            if event.key == 112:
+                shouldTick = not shouldTick
 
     gameDisplay.fill((0, 0, 0))
 
@@ -84,8 +100,9 @@ while not gameExit:
         for j in range(len(f[i])):
             if f[i][j] == cell:
                 pygame.draw.rect(gameDisplay, (255, 255, 255), [i*scale, j*scale, scale, scale])
-    
-    f = step(f[:])[:]
+
+    if shouldTick:
+        f = step(f[:])[:]
 
     pygame.display.update()
     clock.tick(fps)
